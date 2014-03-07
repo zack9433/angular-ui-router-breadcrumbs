127bba8c9b1663439bc30468f2a214ebdd836e7e
zackyang <zack9433@gmail.com>
2014-03-07 09:26:56 +0800

    Modify breadcrumbs logic

--- src/services/breadcrumbs.js
+++ src/services/breadcrumbs.js
@@ -1,33 +1,13 @@
 breadcrumbs.provider('breadcrumbs', function BreadcrumbsProvider() {
   'use strict';
-  var compile;
-
-  function defaultCompiler(currentState) {
-    if(!currentState.breadcrumb) { return null; }
-
-    if(typeof currentState.breadcrumb === 'string') {
-      return {
-        text: currentState.breadcrumb,
-        stateName: currentState.name
-      };
-    } else {
-      return currentState.breadcrumb;
-    }
-  }
-
-  compile = defaultCompiler;
 
   function refresh($state, breadcrumbs) {
-    var currentState = $state.$current,
-      breadcrumb;
+    var currentState = $state.$current;
 
     breadcrumbs.length = 0;
 
-    while(currentState.parent) {
-      breadcrumb = compile(currentState.self);
-      if(breadcrumb) { breadcrumbs.unshift(breadcrumb); }
-      currentState = currentState.parent;
-    }
+    breadcrumbs = currentState.breadcrumb;
+
     return breadcrumbs;
   }
 
@@ -42,9 +22,10 @@ breadcrumbs.provider('breadcrumbs', function BreadcrumbsProvider() {
     '$state',
     function($rootScope, $state) {
       var breadcrumbs = [];
-      refresh($state, breadcrumbs);
-      $rootScope.$on('$stateChangeSuccess', function() { refresh($state, breadcrumbs); });
-      return breadcrumbs;
+      breadcrumbs = refresh($state, breadcrumbs);
+      $rootScope.$on('$stateChangeSuccess', function() {
+        breadcrumbs = refresh($state, breadcrumbs);
+      });
     }
   ];
 });
