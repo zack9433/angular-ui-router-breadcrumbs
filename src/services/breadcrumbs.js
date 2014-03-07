1cfee0da36e0a7a4e6b90a5041587f9350a855bf
zackyang <zack9433@gmail.com>
2014-03-07 11:36:17 +0800

    Modify refresh logic

--- src/services/breadcrumbs.js
+++ src/services/breadcrumbs.js
@@ -2,13 +2,15 @@ breadcrumbs.provider('breadcrumbs', function BreadcrumbsProvider() {
   'use strict';
 
   function refresh($state, breadcrumbs) {
-    var currentState = $state.$current,
-        len = currentState.breadcrumb.length;
-
     breadcrumbs.length = 0;
 
-    for (var i = 0; i < len; i++) {
-      breadcrumbs.push(currentState.breadcrumb[i]);
+    if (currentState.breadcrumb) {
+      var currentState = $state.$current,
+          len = currentState.breadcrumb.length;
+
+      for (var i = 0; i < len; i++) {
+        breadcrumbs.push(currentState.breadcrumb[i]);
+      }
     }
 
     return breadcrumbs;
